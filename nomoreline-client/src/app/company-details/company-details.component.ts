20ac06582cf76523b911d21068e4219aa6aa8df4
Gabriel Gamy <gabrielyakpaoro@gmail.com>
2017-06-09 17:11:46 -0400

    Nomoreline Angular Client  - Refactoring

---
+++ nomoreline-client/src/app/company-details/company-details.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'nomoreline-company-details',
+  templateUrl: './company-details.component.html',
+  styleUrls: ['./company-details.component.css']
+})
+export class CompanyDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
+}
